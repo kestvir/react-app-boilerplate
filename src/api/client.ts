bd3c1dec53eb7146277c7b8fe5d597e9b92b14e2
kestutisvirbickas <v.kestutis123@gmail.com>
2022-10-13 20:04:29 +0300

    Refactor client

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,12 +2,13 @@ export const client = async <T>(
   path: string,
   config?: RequestInit
 ): Promise<T> => {
-  const request = new Request(path, config);
-  const response = await fetch(request);
+  return fetch(path, config).then(async (response) => {
+    if (response.ok) {
+      return await response.json();
+    } else {
+      const errorMessage = await response.text();
 
-  if (response.ok) return await response.json();
-
-  const errorMessage = await response.text();
-
-  return Promise.reject(new Error(errorMessage));
+      return Promise.reject(new Error(errorMessage));
+    }
+  });
 };
